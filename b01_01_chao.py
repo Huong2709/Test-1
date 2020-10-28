ec17e7694d5b02e25406adce5d9f43a55eaf8647
Huong2709 <73605668+Huong2709@users.noreply.github.com>
2020-10-28 23:52:26 +0700

    Add files via upload

---
+++ b01_01_chao.py
@@ -0,0 +1,10 @@
+'''
+    Viết chương trình in ra màn hình lời chào mừng.
+    Ngày tạo:
+    Tác giả:
+'''
+    
+# Ghi chú: 
+# # Xuất nội dung ra màn hình
+# Bấm Ctr + ? để hiện ra dấu thăng và ghi Ghi chú
+print ('Chào mừng bạn đến với PYTHON!')
